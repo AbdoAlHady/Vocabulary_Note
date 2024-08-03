c69e088c54481088480e4a6e7847618c22bee1a4
AbdoAlhady <abdo.agiry90@gmail.com>
2024-08-03 04:33:35 +0300

    - refactor home app bar

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,47 +1,18 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:vocabulary_note/core/theme/app_colors.dart';
+import 'package:vocabulary_note/ui/widgets/home/home_app_bar.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: DefaultTextStyle(
-          style: TextStyle(
-              fontSize: 18.sp,
-              fontWeight: FontWeight.w600,
-              color: Colors.black),
-          child: AnimatedTextKit(
-            repeatForever: true,
-            pause: const Duration(seconds: 2),
-            animatedTexts: [
-              TypewriterAnimatedText('Welcome to Your Vocabulary'),
-              TypewriterAnimatedText('Discover New Words Today'),
-              TypewriterAnimatedText('Unlock New Words Daily'),
-            ],
-            onTap: () {
-              print("Tap Event");
-            },
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Icons.dark_mode_outlined,
-              size: 30,
-              color: AppColors.primary,
-            ),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: const Center(
+    return const Scaffold(
+      appBar: HomeAppBar(),
+      body: Center(
         child: Text('Home Screen'),
       ),
     );
   }
 }
+
---
+++ lib/ui/widgets/home/home_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:vocabulary_note/core/theme/app_colors.dart';
+
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: DefaultTextStyle(
+        style: TextStyle(
+            fontSize: 18.sp,
+            fontWeight: FontWeight.w600,
+            color: Colors.black),
+        child: AnimatedTextKit(
+          repeatForever: true,
+          pause: const Duration(seconds: 2),
+          animatedTexts: [
+            TypewriterAnimatedText('Welcome to Your Vocabulary'),
+            TypewriterAnimatedText('Discover New Words Today'),
+            TypewriterAnimatedText('Unlock New Words Daily'),
+          ],
+          onTap: () {
+          },
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(
+            Icons.dark_mode_outlined,
+            size: 35,
+            color: AppColors.primary,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+  
+  @override
+  Size get preferredSize =>  Size.fromHeight(50.h);
+}
